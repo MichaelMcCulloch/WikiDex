92ebe4216e75f13eac81032348eb2de30f0bd67d
Michael McCulloch <mjm.gitlab@fastmail.com>
2023-11-17 16:46:15 -0700

    Hook up the LLM logic.

--- rust/src/ingest/wikipedia/markup_processor/parse/llm.rs
+++ rust/src/ingest/wikipedia/markup_processor/parse/llm.rs
@@ -1,6 +1,6 @@
 use crate::{
-    ingest::wikipedia::helper::wiki::UnlabledDocument,
-    llm::{LlmInput, LlmService, LlmServiceError, OpenAiService},
+    ingest::wikipedia::helper::wiki::{DescribedTable, UnlabledDocument},
+    llm::{LlmInput, LlmMessage, LlmRole, LlmService, LlmServiceError, OpenAiService},
 };
 
 pub(crate) async fn process_table_to_llm(
@@ -8,8 +8,23 @@ pub(crate) async fn process_table_to_llm(
     client: &OpenAiService,
 ) -> Result<UnlabledDocument, LlmServiceError> {
     let message = LlmInput {
-        system: todo!(),
-        conversation: todo!(),
+        system: String::from("Interpret and summarize the following HTML table in a concise, plain English description."),
+        conversation: vec![LlmMessage{message: table.to_string(), role: LlmRole::User}],
     };
-    client.get_llm_answer(message).await?;
+    let output = client.get_llm_answer(message).await?;
+
+    let response = output
+        .conversation
+        .into_iter()
+        .last()
+        .and_then(|m| Some(m.message))
+        .ok_or(LlmServiceError::EmptyResponse)?;
+
+    Ok(UnlabledDocument::from_str_and_vec(
+        String::new(),
+        vec![DescribedTable {
+            description: response,
+            table: table.to_string(),
+        }],
+    ))
 }
