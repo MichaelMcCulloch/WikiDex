2f44f43d93b572a6a43ac81dec35e57caec6bdea
Michael McCulloch <mjm.gitlab@fastmail.com>
2023-12-15 23:26:56 -0700

    Python - Be Gone.

--- rust/src/openai/mod.rs
+++ rust/src/openai/mod.rs
@@ -8,15 +8,9 @@ mod kind;
 mod protocol;
 mod service;
 
-pub(crate) use builder::{
-    OpenAiDelegateBuilder, OpenAiDelegateBuilderArgument, OpenAiDelegateBuilderWithChat,
-    OpenAiDelegateBuilderWithCompletion, OpenAiDelegateBuilderWithEmbedding,
-};
+pub(crate) use builder::{OpenAiDelegateBuilder, OpenAiDelegateBuilderArgument};
 pub(crate) use delegate::{LanguageServiceServiceArguments, OpenAiDelegate};
-
-pub(crate) use kind::ModelKind;
-
 pub(crate) use error::{EmbeddingServiceError, LlmServiceError};
-
+pub(crate) use kind::ModelKind;
 pub(crate) use protocol::{LlmMessage, LlmRole, PartialLlmMessage};
 pub(crate) use service::{EmbedService, LlmService};
