4a9ffc431e7418a619f2b7b84df5cb26fb0edafe
Michael McCulloch <mjm.gitlab@fastmail.com>
2023-12-16 20:03:03 -0700

    format the file

--- rust/src/ingest/wikipedia/helper/sql.rs
+++ rust/src/ingest/wikipedia/helper/sql.rs
@@ -3,21 +3,19 @@ use super::{
     gzip_helper::decompress_text,
     wiki::{CompressedPage, CompressedPageWithAccessDate, DocumentFragments},
 };
-use crate::{
-    ingest::wikipedia::IngestError,
-    openai::OpenAiDelegate,
-};
-use chrono::NaiveDateTime; 
+use crate::{ingest::wikipedia::IngestError, openai::OpenAiDelegate};
+use chrono::NaiveDateTime;
 use indicatif::ProgressBar;
-use sqlx::{SqliteConnection, SqlitePool}; 
-use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use sqlx::{migrate::MigrateDatabase, SqliteConnection, SqlitePool};
 use std::{
     io::Write,
     path::Path,
     sync::{
-        atomic::{AtomicI64, Ordering}, Arc,
+        atomic::{AtomicI64, Ordering},
+        Arc,
     },
 };
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 
 pub(crate) async fn write_completion_timestamp(
     pool: &SqlitePool,
@@ -81,7 +79,7 @@ pub(crate) async fn populate_markup_db(
         .await
         .map_err(Sqlite)?;
     progress_bar.set_message("Writing Compressed Markup to DB...DONE");
-    Ok(article_count.fetch_add(0, Ordering::SeqCst) )
+    Ok(article_count.fetch_add(0, Ordering::SeqCst))
 }
 
 pub(crate) async fn populate_docstore_db(
@@ -114,7 +112,7 @@ pub(crate) async fn populate_docstore_db(
             "INSERT INTO article (id , title, access_date, modification_date) VALUES (?1, ?2, ?3, ?4)",
             article_id,
             article_title,
-            access_millis, 
+            access_millis,
             modification_millis
         )
         .execute(&mut *connection)
@@ -155,9 +153,9 @@ pub(crate) async fn write_vectorstore(
 
     while let Some((index, embedding)) = rx.recv().await {
         let _rows = sqlx::query!("BEGIN;",)
-        .execute(&mut *connection)
-        .await
-        .map_err(Sqlite)?;
+            .execute(&mut *connection)
+            .await
+            .map_err(Sqlite)?;
 
         let mut v8: Vec<u8> = vec![];
 
@@ -165,22 +163,21 @@ pub(crate) async fn write_vectorstore(
             v8.write_all(&e.to_le_bytes()).map_err(IoError)?;
         }
 
-
-
         let _rows = sqlx::query!(
             "INSERT INTO embeddings (id, gte_small) VALUES (?1, ?2)",
-            index, v8
+            index,
+            v8
         )
         .execute(&mut *connection)
         .await
         .map_err(Sqlite)?;
-     
+
         create_vectors_bar.inc(1);
 
         let _rows = sqlx::query!("COMMIT;",)
-        .execute(&mut *connection)
-        .await
-        .map_err(Sqlite)?;
+            .execute(&mut *connection)
+            .await
+            .map_err(Sqlite)?;
     }
     Ok(())
 }
@@ -190,8 +187,6 @@ pub(crate) async fn populate_vectorstore_db(
     document_count: i64,
     tx: UnboundedSender<(i64, Vec<f32>)>,
 ) -> Result<(), IngestError> {
-    
-
     let openai = Arc::new(openai);
 
     for index in 0..document_count {
@@ -199,18 +194,18 @@ pub(crate) async fn populate_vectorstore_db(
         let openai = openai.clone();
         let mut connection = pool.acquire().await.map_err(Sqlite)?;
         actix_web::rt::spawn(async move {
-            let row = sqlx::query!(
-                "SELECT text FROM document WHERE id == ?1;",index
-            )
-            .fetch_one(&mut *connection)
-            .await
-            .map_err(Sqlite)?; 
+            let row = sqlx::query!("SELECT text FROM document WHERE id == ?1;", index)
+                .fetch_one(&mut *connection)
+                .await
+                .map_err(Sqlite)?;
 
             let bytes = row.text;
             let document = decompress_text(bytes).map_err(IoError)?;
 
-    
-            let embedding = openai.embed(&document).await.map_err(EmbeddingServiceError)?;
+            let embedding = openai
+                .embed(&document)
+                .await
+                .map_err(EmbeddingServiceError)?;
             let _ = tx.send((index, embedding));
             Ok::<(), IngestError>(())
         });
@@ -221,37 +216,29 @@ pub(crate) async fn populate_vectorstore_db(
 pub(crate) async fn database_is_complete(pool: &SqlitePool) -> Result<bool, IngestError> {
     let mut connection = pool.acquire().await.map_err(Sqlite)?;
 
-    let record = sqlx::query!(
-        "SELECT * FROM completed_on;"
-    )
-    .fetch_one(&mut *connection)
-    .await ;
+    let record = sqlx::query!("SELECT * FROM completed_on;")
+        .fetch_one(&mut *connection)
+        .await;
 
- 
     match record {
-        Ok(_) =>  Ok( true),
-        
+        Ok(_) => Ok(true),
+
         Err(_) => Ok(false),
     }
 }
 
-pub(crate) async fn count_elements(
-    pool: &SqlitePool,
-) -> Result<Option<i64>, IngestError> {
+pub(crate) async fn count_elements(pool: &SqlitePool) -> Result<Option<i64>, IngestError> {
     let mut connection = pool.acquire().await.map_err(Sqlite)?;
 
-    let record = sqlx::query!(
-        "SELECT article_count FROM completed_on LIMIT 1;"
-    )
-    .fetch_one(&mut *connection)
-    .await ;
+    let record = sqlx::query!("SELECT article_count FROM completed_on LIMIT 1;")
+        .fetch_one(&mut *connection)
+        .await;
 
     match record {
         Ok(record) => Ok(Some(record.article_count)),
         Err(sqlx::Error::RowNotFound) => Ok(None),
-        Err(e) => Err(Sqlite(e))
+        Err(e) => Err(Sqlite(e)),
     }
-
 }
 pub(crate) async fn obtain_markup(
     pool: &SqlitePool,
@@ -260,27 +247,21 @@ pub(crate) async fn obtain_markup(
     progress_bar.set_message("Obtaining Markup...");
     let mut connection = pool.acquire().await.map_err(Sqlite)?;
 
-    let records = sqlx::query!(
-        "SELECT * FROM wiki_markup;"
-    )
-    .map(|record| {
-        let gzipped_text = record.text;
-        let article_title = record.title;
-        let timestamp_millis = record.access_date;
-        progress_bar.inc(1);
-        CompressedPageWithAccessDate {
-            gzipped_text,
-            article_title,
-            access_date: NaiveDateTime::from_timestamp_millis(timestamp_millis).unwrap(),
-        }
-    })
-    .fetch_all(&mut *connection)
-    .await.map_err(Sqlite)?;
-
-
-
-
-    
+    let records = sqlx::query!("SELECT * FROM wiki_markup;")
+        .map(|record| {
+            let gzipped_text = record.text;
+            let article_title = record.title;
+            let timestamp_millis = record.access_date;
+            progress_bar.inc(1);
+            CompressedPageWithAccessDate {
+                gzipped_text,
+                article_title,
+                access_date: NaiveDateTime::from_timestamp_millis(timestamp_millis).unwrap(),
+            }
+        })
+        .fetch_all(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
 
     progress_bar.set_message("Obtaining Markup...DONE");
     Ok(records)
@@ -292,69 +273,60 @@ pub(crate) async fn obtain_vectors(
     progress_bar.set_message("Obtaining vectors...");
     let mut connection = pool.acquire().await.map_err(Sqlite)?;
 
-    let records = sqlx::query!(
-        "SELECT gte_small FROM embeddings ORDER BY id ASC;"
-    )
-    .map(|record| {
-        let embedding_bytes = record.gte_small;
-        let mut embedding: Vec<f32> = vec![];
-        for f32_bytes in embedding_bytes.chunks_exact(4) {
-            let mut b = [0u8; 4];
-            b.copy_from_slice(f32_bytes);
-            embedding.push(f32::from_le_bytes(b));
-        }
-        progress_bar.inc(1);
-        embedding
-    })
-    .fetch_all(&mut *connection)
-    .await.map_err(Sqlite)?;
+    let records = sqlx::query!("SELECT gte_small FROM embeddings ORDER BY id ASC;")
+        .map(|record| {
+            let embedding_bytes = record.gte_small;
+            let mut embedding: Vec<f32> = vec![];
+            for f32_bytes in embedding_bytes.chunks_exact(4) {
+                let mut b = [0u8; 4];
+                b.copy_from_slice(f32_bytes);
+                embedding.push(f32::from_le_bytes(b));
+            }
+            progress_bar.inc(1);
+            embedding
+        })
+        .fetch_all(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
 
     progress_bar.set_message("Obtaining vectors...DONE");
-    
+
     Ok(records)
 }
 pub(crate) async fn get_sqlite_pool<P: AsRef<Path>>(path: &P) -> Result<SqlitePool, IngestError> {
     let path = path.as_ref();
     let path = path.to_path_buf();
     let path = path
-            .to_str()
-            .map(|path| format!("sqlite:/{path}"))
-            .ok_or(IngestError::DirectoryNotFound(
-                path.to_path_buf(),
-            ))?;
+        .to_str()
+        .map(|path| format!("{path}"))
+        .ok_or(IngestError::DirectoryNotFound(path.to_path_buf()))?;
 
-    SqlitePool::connect(path.as_ref()).await.map_err(Sqlite)
+    if !sqlx::Sqlite::database_exists(&path).await.map_err(Sqlite)? {
+        sqlx::Sqlite::create_database(&path).await.map_err(Sqlite)?;
+    }
+
+    SqlitePool::connect(&path).await.map_err(Sqlite)
 }
 
 pub(crate) async fn init_markup_sqlite_pool(
     connection: &mut SqliteConnection,
 ) -> Result<(), IngestError> {
-
-    let _rows = sqlx::query!(
-        "DROP TABLE IF EXISTS completed_on;",
-    )
-    .execute(&mut *connection)
-    .await
-    .map_err(Sqlite)?;
-    let _rows = sqlx::query!(
-        "DROP TABLE IF EXISTS wiki_markup;",
-    )
-    .execute(&mut *connection)
-    .await
-    .map_err(Sqlite)?;
-    let _rows = sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS wiki_markup ( id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, text BLOB NOT NULL, access_date INTEGER NOT NULL );",
-    )
-    .execute(&mut *connection)
-    .await
-    .map_err(Sqlite)?;
-    let _rows = sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS completed_on ( db_date INTEGER NOT NULL, article_count INTEGER NOT NULL );",
-    )
-    .execute(&mut *connection)
-    .await
-    .map_err(Sqlite)?;
-
+    let _rows = sqlx::query!("DROP TABLE IF EXISTS completed_on;",)
+        .execute(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
+    let _rows = sqlx::query!("DROP TABLE IF EXISTS wiki_markup;",)
+        .execute(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
+    let _rows = sqlx::query!("CREATE TABLE IF NOT EXISTS wiki_markup ( id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, text BLOB NOT NULL, access_date INTEGER NOT NULL );",)
+        .execute(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
+    let _rows = sqlx::query!("CREATE TABLE IF NOT EXISTS completed_on ( db_date INTEGER NOT NULL, article_count INTEGER NOT NULL );",)
+        .execute(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
 
     Ok(())
 }
@@ -362,81 +334,52 @@ pub(crate) async fn init_markup_sqlite_pool(
 pub(crate) async fn init_docstore_sqlite_pool(
     connection: &mut SqliteConnection,
 ) -> Result<(), IngestError> {
-
-let _rows = sqlx::query!(
-"DROP TABLE IF EXISTS completed_on;",
-)
-.execute(&mut *connection)
-.await
-.map_err(Sqlite)?;
-let _rows = sqlx::query!(
-"DROP TABLE IF EXISTS document;",
-)
-.execute(&mut *connection)
-.await
-.map_err(Sqlite)?;
-let _rows = sqlx::query!(
-"DROP TABLE IF EXISTS article;",
-)
-.execute(&mut *connection)
-.await
-.map_err(Sqlite)?;
-let _rows = sqlx::query!(
-"CREATE TABLE IF NOT EXISTS article ( id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, access_date INTEGER NOT NULL, modification_date INTEGER NOT NULL );",
-)
-.execute(&mut *connection)
-.await
-.map_err(Sqlite)?;
-let _rows = sqlx::query!(
-"CREATE TABLE IF NOT EXISTS document ( id INTEGER PRIMARY KEY NOT NULL,  text BLOB NOT NULL,  article INTEGER NOT NULL,  FOREIGN KEY(article) REFERENCES article(id) );",
-)
-.execute(&mut *connection)
-.await
-.map_err(Sqlite)?;
-let _rows = sqlx::query!(
-"CREATE TABLE IF NOT EXISTS completed_on ( db_date INTEGER NOT NULL, article_count INTEGER NOT NULL );",
-)
-.execute(&mut *connection)
-.await
-.map_err(Sqlite)?;
-
-
+    let _rows = sqlx::query!("DROP TABLE IF EXISTS completed_on;",)
+        .execute(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
+    let _rows = sqlx::query!("DROP TABLE IF EXISTS document;",)
+        .execute(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
+    let _rows = sqlx::query!("DROP TABLE IF EXISTS article;",)
+        .execute(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
+    let _rows = sqlx::query!("CREATE TABLE IF NOT EXISTS article ( id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, access_date INTEGER NOT NULL, modification_date INTEGER NOT NULL );",)
+        .execute(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
+    let _rows = sqlx::query!("CREATE TABLE IF NOT EXISTS document ( id INTEGER PRIMARY KEY NOT NULL,  text BLOB NOT NULL,  article INTEGER NOT NULL,  FOREIGN KEY(article) REFERENCES article(id) );",)
+        .execute(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
+    let _rows = sqlx::query!("CREATE TABLE IF NOT EXISTS completed_on ( db_date INTEGER NOT NULL, article_count INTEGER NOT NULL );",)
+        .execute(&mut *connection)
+        .await
+        .map_err(Sqlite)?;
 
     Ok(())
 }
 pub(crate) async fn init_temp_embedding_sqlite_pool(
     connection: &mut SqliteConnection,
 ) -> Result<(), IngestError> {
-
-
-    let _rows = sqlx::query!(
-        "DROP TABLE IF EXISTS completed_on;",
-        )
+    let _rows = sqlx::query!("DROP TABLE IF EXISTS completed_on;",)
         .execute(&mut *connection)
         .await
         .map_err(Sqlite)?;
-    let _rows = sqlx::query!(
-        "DROP TABLE IF EXISTS embeddings;",
-        )
+    let _rows = sqlx::query!("DROP TABLE IF EXISTS embeddings;",)
         .execute(&mut *connection)
         .await
         .map_err(Sqlite)?;
-    let _rows = sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS embeddings ( id INTEGER PRIMARY KEY NOT NULL, gte_small BLOB NOT NULL);",
-        )
+    let _rows = sqlx::query!("CREATE TABLE IF NOT EXISTS embeddings ( id INTEGER PRIMARY KEY NOT NULL, gte_small BLOB NOT NULL);",)
         .execute(&mut *connection)
         .await
         .map_err(Sqlite)?;
-    let _rows = sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS completed_on ( db_date INTEGER NOT NULL, article_count INTEGER NOT NULL);",
-        )
+    let _rows = sqlx::query!("CREATE TABLE IF NOT EXISTS completed_on ( db_date INTEGER NOT NULL, article_count INTEGER NOT NULL);",)
         .execute(&mut *connection)
         .await
         .map_err(Sqlite)?;
-        
-
-
 
     Ok(())
 }
-
