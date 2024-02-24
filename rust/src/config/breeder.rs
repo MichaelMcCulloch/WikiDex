10cf683a3e2adfd6d8771988faee461308d49437
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-02-23 18:41:37 -0700

    It actually does work

--- rust/src/config/breeder.rs
+++ rust/src/config/breeder.rs
@@ -11,6 +11,7 @@ pub(crate) struct Config {
     pub(crate) output_directory: PathBuf,
     pub(crate) embed_url: Url,
     pub(crate) embed_model_name: PathBuf,
+    pub(crate) openai_key: Option<String>,
     pub(crate) llm_url: Url,
     pub(crate) language_model_name: PathBuf,
     pub(crate) language_model_kind: ModelKind,
@@ -31,6 +32,7 @@ impl From<BreederArgs> for Config {
             docstore: value.docstore,
             thinking_styles_db: value.thinking_styles_db,
             mutation_prompts_db: value.mutation_prompts_db,
+            openai_key: value.openai_key,
         }
     }
 }
