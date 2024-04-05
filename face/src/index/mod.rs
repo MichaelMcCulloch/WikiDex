88ca4569081918054822215a2cfa335a65f69217
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-04-04 20:18:51 -0600

    remove face from repo

--- face/src/index/mod.rs
+++
@@ -1,7 +0,0 @@
-mod error;
-mod faiss_index;
-mod service;
-
-pub(crate) use error::{IndexError, IndexSearchError};
-pub(crate) use faiss_index::FaissIndex;
-pub(crate) use service::SearchService;
--- face_api/src/models/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod neighbors;
-pub use self::neighbors::Neighbors;
-pub mod query;
-pub use self::query::Query;
