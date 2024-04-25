818e88eae1f7f0baef25e36d9ba055c4c4872dae
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-04-25 10:57:11 -0600

    Clippy fix

---
+++ wikidex/run_ingest copy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+export DATABASE_URL="sqlite:///home/michael/Development/Omnipedia Inc./wikidex/wikidex/sqlite_dummy.db"
+export CUDA="/opt/cuda";
+export CC="$CUDA/bin/gcc";
+export CXX="$CUDA/bin/g++";
+export RUST_LOG=info
+export RUSTFLAGS="-C target-cpu=native"
+
+x=`(cargo clippy --fix --workspace --message-format=json --all-targets --allow-dirty)`
+success=$(echo "$x" | jq -r '.success' | grep -v "null" | tail -n 1)
+# Check if the "success" field is true
+if [ "$success" = "true" ]; then
+    git add .
+    git commit -m "Clippy fix"
+fi
+
+echo $x
