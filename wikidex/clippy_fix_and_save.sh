a45a0c24ed30524f084880c89ec4d42aab1d3d5a
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-04-28 13:18:10 -0600

    Clippy fix

--- wikidex/clippy_fix_and_save.sh
+++ wikidex/clippy_fix_and_save.sh
@@ -5,15 +5,15 @@ export CC="$CUDA/bin/gcc";
 export CXX="$CUDA/bin/g++";
 export RUST_LOG=info
 export RUSTFLAGS="-C target-cpu=native"
-cargo clippy --fix --workspace --message-format=json --all-targets --allow-dirty
-x=`(cargo clippy --fix --workspace --message-format=json --all-targets --allow-dirty)`
-(
-    success=$(echo "$x" | jq -r '.success' | grep -v "null" | tail -n 1)
-    # Check if the "success" field is true
-    if [ "$success" = "true" ]; then
-        git add .
-        git commit -m "Clippy fix"
-    fi
+# cargo clippy --fix --workspace --message-format=json --all-targets --allow-dirty &
+(x=`(cargo clippy --fix --workspace --message-format=json --all-targets --allow-dirty)`
+    (
+        success=$(echo "$x" | jq -r '.success' | grep -v "null" | tail -n 1)
+        # Check if the "success" field is true
+        if [ "$success" = "true" ]; then
+            git add .
+            git commit -m "Clippy fix"
+        fi
+    )
 )&
-
-echo $x
+wait
