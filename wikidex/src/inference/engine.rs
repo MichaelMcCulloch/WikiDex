9735cade7fe6ca7fa5375d0a3285ae444d644f8c
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-04-28 13:20:34 -0600

    Clippy fix

--- wikidex/src/inference/engine.rs
+++ wikidex/src/inference/engine.rs
@@ -129,7 +129,7 @@ impl Engine {
         match messages.into_iter().last() {
             Some(Message::User(user_query)) => {
                 let documents = self.get_documents(&user_query).await?;
-                let prompt = self.format_rag_template(&documents, &user_query)?;
+                let _prompt = self.format_rag_template(&documents, &user_query)?;
 
                 let sources = organize_sources(documents, num_sources);
 
@@ -190,7 +190,7 @@ impl Engine {
                     let _ = tx.send(PartialMessage::done().message());
                 });
 
-                let llm_service_arguments = LanguageServiceArguments { prompt: todo!() };
+                let llm_service_arguments = LanguageServiceArguments { prompt: "" };
                 self.llm_client
                     .stream_llm_answer(
                         llm_service_arguments,
