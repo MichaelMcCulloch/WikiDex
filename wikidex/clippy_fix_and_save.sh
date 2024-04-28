734b48b8607ddee5c83df7b174e24adfd97aab62
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-04-28 13:33:18 -0600

    Clippy fix

--- wikidex/clippy_fix_and_save.sh
+++ wikidex/clippy_fix_and_save.sh
@@ -5,4 +5,4 @@ export CC="$CUDA/bin/gcc";
 export CXX="$CUDA/bin/g++";
 export RUST_LOG=info
 export RUSTFLAGS="-C target-cpu=native"
-cargo clippy --fix --workspace --message-format=json --all-targets --allow-dirty &&   git add . && git commit -m "Clippy fix"
+cargo clippy --fix --workspace --message-format=json --all-targets --allow-dirty && git checkout local-step && git add . && git commit -m "Clippy fix"
