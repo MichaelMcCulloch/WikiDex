258f9ba1cd5f813f8c6cbbe3953ad99a4ec45072
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-04-30 08:00:06 -0600

    Tue Apr 30 08:00:06 AM MDT 2024

--- wikidex/src/main.rs
+++ wikidex/src/main.rs
@@ -1,24 +1,27 @@
 #[cfg(all(feature = "sqlite", feature = "postgres"))]
 compile_error!("features `sqlite` and `postgres` are mutually exclusive");
 
-mod cli_args;
 mod config;
 #[cfg(feature = "server")]
 mod docstore;
-mod embedding_client;
 #[cfg(feature = "server")]
 mod formatter;
 #[cfg(feature = "server")]
 mod index;
 #[cfg(feature = "server")]
 mod inference;
-#[cfg(feature = "ingest")]
-mod ingest;
-mod llm_client;
 #[cfg(feature = "server")]
 mod server;
+
+#[cfg(feature = "ingest")]
+mod ingest;
+
 #[cfg(test)]
 mod test_data;
+
+mod cli_args;
+mod embedding_client;
+mod llm_client;
 
 use crate::embedding_client::EmbeddingClient;
 #[cfg(feature = "ingest")]
