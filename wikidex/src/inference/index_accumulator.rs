b06d438491dbac08f802a4893eff44f97fe97515
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-05-04 15:05:55 -0600

    Sat May  4 03:05:55 PM MDT 2024

--- wikidex/src/inference/index_accumulator.rs
+++ wikidex/src/inference/index_accumulator.rs
@@ -4,3 +4,7 @@ pub(crate) struct IndexAccumulator {
     dictionary: HashMap<i64, u8>,
     token_buffer: Vec<String>,
 }
+
+pub(crate) trait IndexAccumulatorTrait {
+    fn token(&mut self, token: &str) -> Option<&str>;
+}
