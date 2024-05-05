f6608daf106715f084a903c6eee2d83771a7c44c
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-05-04 22:17:44 -0600

    Become dependent you will

--- wikidex/clippy_fix_and_save.sh
+++ wikidex/clippy_fix_and_save.sh
@@ -5,9 +5,10 @@
 # export CXX="$CUDA/bin/g++";
 # export RUST_LOG=info
 # export RUSTFLAGS="-C target-cpu=native"
-cargo clippy --fix --workspace --message-format=json --all-targets --allow-dirty && \
-git checkout local-step && \
-git add . && \
-git commit -m "`date`"
+cargo clippy --fix --workspace --message-format=json --all-targets --allow-dirty
+# Become dependent you will
 # && \
+# git checkout local-step && \
+# git add . && \
+# git commit -m "`date`" && \
 # git push
