e4d48e50d0e24617bad8e1eb0a6b628bc9568958
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-05-04 21:58:23 -0600

    Sat May  4 09:58:23 PM MDT 2024

--- wikidex/src/inference/index_accumulator.rs
+++ wikidex/src/inference/index_accumulator.rs
@@ -306,14 +306,14 @@ mod test {
         assert_eq!(TVS::Nothing, a.token("1"));
         assert_eq!(TVS::Nothing, a.token("2"));
         assert_eq!(TVS::Nothing, a.token("3"));
-        assert_eq!(TVS::Nothing, a.token("4 "));
         assert_eq!(
             TVS::Twofer(
                 TV::Transform("[0](http://localhost/#0)".to_string(), 0),
                 TV::NoOp(" ")
             ),
-            a.flush()
+            a.token("4 ")
         );
+        assert_eq!(TVS::Nothing, a.flush());
     }
     #[test]
     fn index_matched_leading_trailing() {
