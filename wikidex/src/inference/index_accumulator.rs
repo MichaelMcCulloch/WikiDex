f548e5b319e95de20b63272289b1a13bb404cebf
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-05-04 18:21:56 -0600

    Sat May  4 06:21:56 PM MDT 2024

--- wikidex/src/inference/index_accumulator.rs
+++ wikidex/src/inference/index_accumulator.rs
@@ -134,6 +134,6 @@ mod test {
         assert_eq!(I::Nothing, a.token("1"));
         assert_eq!(I::Nothing, a.token("2"));
         assert_eq!(I::Nothing, a.token("3"));
-        assert_eq!(Some("1".to_string()), a.flush());
+        assert_eq!(Some("0".to_string()), a.flush());
     }
 }
