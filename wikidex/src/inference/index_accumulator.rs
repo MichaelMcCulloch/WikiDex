c4750478911ebbce4400e7f56a9408b13bf73422
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-05-04 22:16:08 -0600

    Sat May  4 10:16:08 PM MDT 2024

--- wikidex/src/inference/index_accumulator.rs
+++ wikidex/src/inference/index_accumulator.rs
@@ -440,13 +440,4 @@ mod test {
         assert_eq!(TVS::Unit(TV::NoOp("i34i")), a.token("i34i"));
         assert_eq!(TVS::Nothing, a.flush());
     }
-    #[test]
-    fn index_matched_letters_large_fragments() {
-        let mut a = IndexAccumulator::new(vec![123], 0, Box::new(formatter));
-
-        assert_eq!(TVS::Unit(TV::NoOp("i123i")), a.token("i123i"));
-        assert_eq!(TVS::Unit(TV::NoOp("i12i")), a.token("i12i"));
-        assert_eq!(TVS::Unit(TV::NoOp("i34i")), a.token("i34i"));
-        assert_eq!(TVS::Nothing, a.flush());
-    }
 }
