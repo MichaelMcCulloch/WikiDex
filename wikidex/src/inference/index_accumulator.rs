6457ec9190d2eaa7020dd85b095ffea196aa8f8c
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-05-04 21:54:08 -0600

    Sat May  4 09:54:08 PM MDT 2024

--- wikidex/src/inference/index_accumulator.rs
+++ wikidex/src/inference/index_accumulator.rs
@@ -231,7 +231,10 @@ mod test {
         assert_eq!(TVS::Nothing, a.token("2"));
         assert_eq!(TVS::Nothing, a.token("3"));
         assert_eq!(
-            TVS::Unit(TV::Transform("[0](http://localhost/#0) ".to_string(), 0)),
+            TVS::Twofer(
+                TV::Transform("[0](http://localhost/#0) ".to_string(), 0),
+                TV::NoOp(" ")
+            ),
             a.token(" ")
         );
         assert_eq!(TVS::Nothing, a.token("1"));
