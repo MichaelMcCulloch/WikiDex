7afa5a85b57aa0dd4ddf3ac3402322a0f2435e7e
MichaelMcCulloch <michael.james.mcculloch@fastmail.com>
2024-05-08 20:49:05 -0600

    run ingest instead of test

--- wikidex/run_ingest.sh
+++ wikidex/run_ingest.sh
@@ -7,32 +7,32 @@ export RUST_LOG="info,async_openai=error"
 export RUSTFLAGS="-C target-cpu=native"
 
 # cargo test --package wikidex --bin wikidex -- ingest::pipeline::processor::test::test --exact --show-output  --nocapture
-cargo test --package wikidex --bin wikidex -- ingest::pipeline::index_converter::test::test --exact --show-output  --nocapture
-# cargo run --release -- \
-# wikipedia \
-# --wiki-xml \
-# "/home/michael/Development/Scratch Space/wikisql/enwiki-20240420-pages-articles.xml" \
-# --output-directory \
-# "/home/michael/Development/Scratch Space/wikisql/" \
-# --ingest-limit \
-# "0" \
-# --embed-name \
-# "thenlper/gte-small" \
-# --embed-url \
-# "http://192.168.1.120:9000/v1" \
-# --embed-endpoint \
-# openai \
-# --llm-name \
-# "TheBloke/Mistral-7B-Instruct-v0.2-AWQ" \
-# --llm-url \
-# "http://triton:8001" \
-# --llm-endpoint \
-# triton \
-# --llm-kind \
-# instruct \
-# --nebula-url \
-# "http://graphd:9669" \
-# --nebula-user \
-# "root" \
-# --nebula-pass \
-# "nebula"
+# cargo test --package wikidex --bin wikidex -- ingest::pipeline::index_converter::test::test --exact --show-output  --nocapture
+cargo run --release -- \
+wikipedia \
+--wiki-xml \
+"/home/michael/Development/Scratch Space/wikisql/enwiki-20240420-pages-articles.xml" \
+--output-directory \
+"/home/michael/Development/Scratch Space/wikisql/" \
+--ingest-limit \
+"0" \
+--embed-name \
+"thenlper/gte-small" \
+--embed-url \
+"http://192.168.1.120:9000/v1" \
+--embed-endpoint \
+openai \
+--llm-name \
+"TheBloke/Mistral-7B-Instruct-v0.2-AWQ" \
+--llm-url \
+"http://triton:8001" \
+--llm-endpoint \
+triton \
+--llm-kind \
+instruct \
+--nebula-url \
+"http://graphd:9669" \
+--nebula-user \
+"root" \
+--nebula-pass \
+"nebula"
